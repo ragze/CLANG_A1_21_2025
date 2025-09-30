67ccc32579d1d464c5d34ecf95f23f96d95b5b28
ragze <2025a6r021@mietjammu.in>
2025-09-30 12:32:37 +0530

    Update Largestnum1.c

--- Largestnum1.c
+++ Largestnum1.c
@@ -3,6 +3,10 @@ int main(){
     int num1,num2;
     printf("Enter the two numbers : ");
     scanf("%d%d",&num1,&num2);
-    (num1>num2)?printf("%d is largest",num1):printf("%d is largest",num2);
+    if(num1>num2)
+        printf("%d is largest",num1);
+    else
+        printf("%d is largest",num2);
     return 0;
 }
+
