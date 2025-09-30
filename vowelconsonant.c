81446087c33629f6b1e7c33e7c48a1ceac6729f3
ragze <2025a6r021@mietjammu.in>
2025-09-30 13:10:11 +0530

    Create vowelconsonant.c

---
+++ vowelconsonant.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char ch;
+    scanf(" %c", &ch);
+    
+    if(isalpha(ch)){
+        ch = toupper(ch);
+        if (ch==65||ch==69||ch==73||ch==79||ch==85)
+        printf("Vowel");
+        else if ((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+        printf("Consonant");
+    }else {
+        printf("Invalid Input");
+    return 0;
+    }
+}
